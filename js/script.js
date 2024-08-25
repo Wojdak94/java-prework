51262a2ca75267639f287acad0e9fde43e80b1c1
Filip Wojdak <WojdakFilip94@gmail.com>
2024-08-25 19:43:14 +0200

    Game

--- js/script.js
+++ js/script.js
@@ -21,11 +21,24 @@ let playerMove = 'nieznany ruch';
 
 if(playerInput == '1'){
   playerMove = 'kamień';
-} else if (playerInput == '2'){
+} 
+if (playerInput == '2'){
     playerMove = 'papier'
-} else if (playerInput == '3'){
+} 
+if (playerInput == '3'){
     playerMove = 'nożyce'
 }
 
 printMessage('Twój ruch to: ' + playerMove);
 
+if( computerMove == 'kamień' && playerMove == 'papier'){
+    printMessage('Ty wygrywasz!');
+  } else if( computerMove == 'papier' && playerMove == 'nożyce'){
+    printMessage('Ty wygrywasz!');
+  } else if( computerMove == 'nożyce' && playerMove == 'kamień'){
+    printMessage('Ty wygrywasz!');
+  } else if( playerMove == 'nieznany ruch'){
+    printMessage('podano zły numer');
+  } else {
+    printMessage('Przegrałeś')
+  }
