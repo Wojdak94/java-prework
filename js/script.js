4964c4101fe363f5deca0144206830f34fc98690
Filip Wojdak <WojdakFilip94@gmail.com>
2024-08-25 19:24:14 +0200

    komputerMove

--- js/script.js
+++ js/script.js
@@ -1,13 +1,14 @@
-function printMessage(msg){
-	let div = document.createElement('div');
-	div.innerHTML = msg;
-	document.getElementById('messages').appendChild(div);
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+
+if(randomNumber == 1){
+  computerMove = 'kamień';
+} else if (randomNumber == '2'){
+    computerMove = 'papier'
+} else if (randomNumber == '3'){
+    computerMove = 'nożyce'
 }
 
-function clearMessages(){
-	document.getElementById('messages').innerHTML = '';
-}
-
-let playerMove = 'paper' ;
-
-printMessage('zagrałem' + playerMove + '! Jeśli Twój ruch to nożyczki to wygrywasz!');
+printMessage('Mój ruch to: ' + computerMove);
